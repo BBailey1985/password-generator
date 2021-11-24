a26ecbd78251ce729b514731ea191e65b17c2d0a
BBailey1985 <brandon_bailey@hotmail.com>
2021-11-23 19:31:47 -0600

    saving before class

--- Develop/script.js
+++ Develop/script.js
@@ -1,38 +1,37 @@
 var generateBtn = document.querySelector("#generate");
 
+
 var generatePassword = function() {
-  //prompt for password length
-  var promptLength = window.prompt("Choose length of password between 8 and 128 characters");
-  console.log();
-  // validate promptLength answer
+//   var passwordLength = {
+//     from: 8,
+//     to: 128,
+//   };
+//   var passwordCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 !#$%&'*-/:<=>?@^_`~";
+//   var password = "";
+//   //prompt for password length
+//   var passwordLength = window.prompt("Choose length of password between 8 and 128 characters");
+//   // validate promptLength answer
 
-  if (promptLength === "" || promptLength === null) {
-    window.alert("Response not valid. Please try again.");
-  }
-  else if (promptLength < 8)
-  // confirm("Confirm you want to include lowercase characters?");
-  // confirm("Confirm you want to include uppercase characters?");
-  // confirm("Confirm you want to include numeric characters?");
-  // confirm("Confirm you want to include special characters?");
+//   if (passwordLength === "" || passwordLength === null) {
+//     window.alert("Response not valid. Please try again.");
+//   }
+//   else if (passwordLength > 8 || passwordLength < 128) {
+//   }
+//   else {
+//     window.alert("Invalid Response");
+//     return false;
+//   }
+//   // var promptLower = window.confirm("Do you want the password to include lowercase letters?");
+//   // var promptUpper = window.confirm("Do you want the password to include uppercase letters?");
+//   // var promptNum = window.confirm("Do you want the password to include numeric characters?");
+//   // var promptSpecial = window.confirm("Do you want the password to include special characters?");
 
-  // validation of input
-
-
-  // generate password based on validated data
-
-
-
-  //display password
-
-
-
-
-
-
-
-  return "true"
-
-}
+//   for (var i = 0; i < passwordLength; i++) {
+//     var randomNumber = Math.floor(Math.random() * passwordCharacters.length);
+//     password +=passwordCharacters.substring(randomNumber, randomNumber +1);
+//   }
+//     document.getElementById("password");
+// }
 
 // Write password to the #password input
 function writePassword() {
@@ -44,4 +43,4 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword)
